549748a12d148c443555e405dd8825841f126fdd
Christopher Rabotin <christopher.rabotin@gmail.com>
2025-06-11 23:38:17 -0600

    Not quite there yet with cmake
    https://github.com/david-cattermole/cxx-demo-example/blob/main/build_linux.bash

---
+++ anise-cpp/cpp/tests/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "---- Start Testing..." << std::endl;
+
+    int x = 2 + 2;
+    std::cout << "2 + 2 = " << x << std::endl;
+
+    std::cout << "---- Finished Testing!" << std::endl;
+    return 0;
+}
